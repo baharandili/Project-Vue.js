50fc23222773fe69b8f6e89c7dbc5602d1ba6af8
baharandili <baharandili05@gmail.com>
2024-01-22 17:07:38 +0700

    Update Class Dynamic

--- post/src/components/childs.vue
+++ post/src/components/childs.vue
@@ -4,50 +4,60 @@
     <!-- <h3>{{ users.name }}</h3>
     <h3>{{ users.email }}</h3>
     <button v-on:click="getData()"> Click Disini Yaa </button> -->
-    <Newuser/>
-       <ul>
+       <!-- <ul>
         <li v-for="item in user" :key="item.name"> 
-            <!-- {{ item.name }} ,
-            {{ item.email }}  -->
+            {{ item.name }} ,
+            {{ item.email }} 
             <h2>{{ item.name }}</h2>
             <h2>{{ item.email }}</h2>
             <Newuser :data="item"/>
         </li>
     </ul>
+    <div>
+    <h1 v-html="emls"></h1>
+    <div v-html="id"></div>
+    </div> -->
+    <h3 :class="{aqua:color}">Class Dynamic</h3>
+    <button v-on:click="color=!color">Click Here</button>
+    <Newuser/>
 </template>
 <script>
 import Newuser from './newuser.vue';
 export default {
     name: "childs_com",
-    // props: {
-    //     name: String,
-    //     users: Object,
-    //     getData: Function,
-    // },
+    props: {
+        name: String,
+        user: Object,
+        getData: Function,
+    },
     data() {
         return {
-            user: [
-                {
-                    name: 'Jaks',
-                    email: 'jamali1@gmail.com'
-                },
-                {
-                    name: 'Jams',
-                    email: 'jamali2@gmail.com'
-                },
-                {
-                    name: 'Joks',
-                    email: 'jamali3@gmail.com'
-                },
-                {
-                    name: 'Kaml',
-                    email: 'jamali4@gmail.com'
-                },
-                {
-                    name: 'yogs',
-                    email: 'jamali5@gmail.com'
-                }
-            ]
+            // user: [
+            //     {
+            //         name: 'Jaks',
+            //         email: 'jamali1@gmail.com'
+            //     },
+            //     {
+            //         name: 'Jams',
+            //         email: 'jamali2@gmail.com'
+            //     },
+            //     {
+            //         name: 'Joks',
+            //         email: 'jamali3@gmail.com'
+            //     },
+            //     {
+            //         name: 'Kaml',
+            //         email: 'jamali4@gmail.com'
+            //     },
+            //     {
+            //         name: 'yogs',
+            //         email: 'jamali5@gmail.com'
+            //     }
+            // ]
+            // names:'jamal',
+            // emls:'<h3>jams3@gmail.com</h3>',
+            // id: 82244770086,
+            color :false
         };
     },
     
@@ -55,7 +65,12 @@ export default {
 };
 </script>
 <style>
-h3{
-    color:aqua,
+.aqua{
+    color:blue;
+    font-weight: bold;
+    background-color: aqua;
+    border-color: black;
+    width: 160px;
+    height: 20px;
 }
 </style>
